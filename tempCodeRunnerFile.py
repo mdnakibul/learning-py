ce6e2dad6ce6bed52f8486a23fac9ed252413959
Md Nakibul Hosen Nahid Nahid <nakibulhosen7@gmail.com>
2024-11-25 18:00:00 +0600

    test code for other program

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+filePath = '../tryJS/Hello world.txt'
+# with open('../tryJS/Hello world.txt','r+') as file:
+#     fileContent = file.read()
+#     print('initital file content', fileContent)
+#     file.seek(0)
+#     file.writelines('Hello World! \n Python is awesome!\n')
+#     file.truncate()
+#     file.seek(0)
+#     newFileContent = file.read()
+#     print('New file content:\n', newFileContent)
