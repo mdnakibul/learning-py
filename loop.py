4740e5791495f25396ae0cafc70f332cce89cc4d
Md Nakibul Hosen Nahid Nahid <nakibulhosen7@gmail.com>
2024-11-25 17:57:30 +0600

    learn about python loops

---
+++ loop.py
@@ -0,0 +1,68 @@
+# for i in range(1,11):
+#     print('Hello World')
+
+# for j in range(1,11):
+#     print(j)
+
+# s = 'Hello World'
+# for c in s:
+#     print(c)
+
+# skip a number
+# for i in range(2,10,2):
+#     print(i)
+
+# loop over a list 
+# list1 = [1,2,3,4,5]
+# for integer in list1:
+#     print(integer)
+
+# fruits = ["apple", "banana", "cherry"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# for i in range(1,4):
+#     for j in range(1,3):
+#         print("i = ", i, "j = ", j)
+
+# iterate over an array of object 
+# student_scores = {"John": 85, "Jane": 92, "Mike": 78}
+
+# Iterating over keys and values
+# for student, score in student_scores.items():
+#     print(f"{student}: {score}")
+
+# count = 0
+# while(count < 5):
+#     print(count)
+#     count+=1
+# break 
+s = 'HSTU, Dinajpur'
+# for c in s:
+#     if(c == 'a'):
+#         break
+#     print(c)
+
+# continue 
+# for c in s:
+#     if(c == 'a'):
+#         continue
+#     print(c)
+
+# pass 
+# for c in s:
+#     pass
+
+# number divisable by 3 and 5
+
+# for i in range(1,21):
+#     if(i%3 == 0 or i%5 == 0):
+#         print(i)
+
+j = 1
+while(j < 21):
+    if(j%3 == 0 or j%5 == 0):
+        print(j)
+    j = j + 1
+
